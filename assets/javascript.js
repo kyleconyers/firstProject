9d06f723009c862cd92577dd52cd0bbb029f53bb
Kevin <kphyde@gmail.com>
2018-09-27 19:09:33 -0700

    move #container styles to css doc, move JavaScript map creation from script tag in html to JS file

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,11 @@
 var state;
 var year;
 var states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
-// var map = new Datamap({
-//     element: document.getElementById('container'),
-//     scope: 'usa'
-// });
+// INITIALIZES THE MAP OF THE USA ON TO THE PAGE
+var map = new Datamap({
+    element: document.getElementById('container'),
+    scope: 'usa'
+});
 
 $("#submit-button").on("click", function () {
     event.preventDefault();
